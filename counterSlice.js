97eefb93455e345c588aa87eeace810187635086
rishi14052003 <143413225+rishi14052003@users.noreply.github.com>
2025-09-16 17:06:12 +0530

    Update counterSlice.js

--- counterSlice.js
+++ counterSlice.js
@@ -1,22 +1,4 @@
-/* this is basic program for counter +1,-1,reset to zero and to increase by a multiple of number
-import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { value: 0 };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment: (state) => { state.value += 1 },
-    decrement: (state) => { state.value -= 1 },
-    reset: (state) => { state.value = 0 },
-    incrementByAmount: (state, action) => { state.value += action.payload },
-  },
-});
-
-export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
-export default counterSlice.reducer;
-*/
 
 //TO-DO LIST USING REACT REDUX
 
@@ -44,4 +26,5 @@ const counterSlice = createSlice({
 
 export const { addTodo, deleteTodo, editTodo } = counterSlice.actions;
 export default counterSlice.reducer;
+
 
