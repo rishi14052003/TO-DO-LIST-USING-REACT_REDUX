91016e3ce91a49fadba5455d5395efeab0bb542b
rishi14052003 <143413225+rishi14052003@users.noreply.github.com>
2025-09-16 17:06:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-/* this is basic program for counter +1,-1,reset to zero and to increase by a multiple of number
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-*/
 
 //TO-DO LIST USING REACT REDUX
 
